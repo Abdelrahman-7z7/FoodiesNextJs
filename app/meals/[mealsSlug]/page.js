6504191d8cd13a9b1392c20befdff2beb8e9fc5b
Abdelrahman Reyad <92989840+Abdelrahman-7z7@users.noreply.github.com>
2025-04-23 12:46:52 +0300

    add the getMeal function to the meals.js file taking into consideration the SQL-Injection, adding the mealsSlug page.js content and the style , making if-condition for looking to the nearest not-found page or error page if there is a drawback of the fetching content

--- app/meals/[mealsSlug]/page.js
+++ app/meals/[mealsSlug]/page.js
@@ -1,8 +1,42 @@
-export default function slugPage(){
+import { notFound } from 'next/navigation'
+import Image from 'next/image'
+import { getMeal } from '@/lib/meals'
+
+import classes from './page.module.css'
+export default async function slugPage({params}){
+const meal = await getMeal(params.mealsSlug)
+
+    //if we are looking for non-existing meal we can navigate for the closest notFoundPage (the notFound) here looks for the closest notFound or error page
+    if(!meal){
+        notFound();
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br/>')
+    
     return(
-        <main>
-            <h1>Slug Page</h1>
-            <p>This is a slug page.</p>
-        </main>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill/>
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p> 
+                    <p className={classes.Summary}>
+                        {meal.summary}
+                    </p>
+                </div>
+            </header>
+            <main>
+                <p 
+                    className={classes.instructions}
+                    dangerouslySetInnerHTML={{
+                        __html: meal.instructions,
+                    }}
+                ></p>
+            </main>
+        </>
     )
 }
