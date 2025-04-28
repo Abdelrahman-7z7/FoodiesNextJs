1633f1fd1d04d1a553070552d11088d098555a15
Abdelrahman Reyad <92989840+Abdelrahman-7z7@users.noreply.github.com>
2025-04-28 12:18:20 +0300

    using the 'use server' in the same file as frontend to make some actions on the form data

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,25 @@ import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
+
+    //when we try to add a server action of CRUD operations we follow the next
+    //when the form gets uploaded the formData prop will be passed to the shareMeal function containing the form data
+    async function shareMeal(formData){
+        'use server';
+
+        //sanitizing data for blocking SQL-INJECTION
+        const meal = {
+            title: formData.get('title'),
+            summary: formData.get('summary'),
+            instructions: formData.get('instructions'),
+            image: formData.get('image'),
+            creator: formData.get('name'),
+            creator_email: formData.get('email')
+        }
+
+        console.log(meal)
+    }
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +30,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +58,8 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker></ImagePicker>
+          {/* we must sit the right name and label for the image to be used later in the fromData */}
+          <ImagePicker label="Your image" name="image"></ImagePicker>
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
