519d4d1ce5dc8971268ba13e35259aecde6eed68
Abdelrahman Reyad <92989840+Abdelrahman-7z7@users.noreply.github.com>
2025-04-28 13:55:33 +0300

    extracting the use server actions to prevent the use client interferance

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,25 +1,9 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
+
 
 export default function ShareMealPage() {
-
-    //when we try to add a server action of CRUD operations we follow the next
-    //when the form gets uploaded the formData prop will be passed to the shareMeal function containing the form data
-    async function shareMeal(formData){
-        'use server';
-
-        //sanitizing data for blocking SQL-INJECTION
-        const meal = {
-            title: formData.get('title'),
-            summary: formData.get('summary'),
-            instructions: formData.get('instructions'),
-            image: formData.get('image'),
-            creator: formData.get('name'),
-            creator_email: formData.get('email')
-        }
-
-        console.log(meal)
-    }
 
   return (
     <>
